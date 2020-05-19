e875cadc28ec2fd94adfcdce0780fb95ef0ccc70
Mustafa Kurukamci <isbuan51@gmail.com>
2020-05-19 13:19:13 +0200

    added contact-page/SPA part 1

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Home from './Home';
 import Menu from './Menu';
+import Contact from './Contact';
 import DishDetail from './DishDetail';
 import Header from './Header';
 import Footer from './Footer';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -12,12 +16,23 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={
+            this.state.promotions.filter((promotion) => promotion.featured)[0]
+          }
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
     };
     return (
       <div>
@@ -29,6 +44,7 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
