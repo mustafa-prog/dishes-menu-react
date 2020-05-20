297589c12161ff9185891d9d34578fa262be0fa6
Mustafa Kurukamci <isbuan51@gmail.com>
2020-05-20 18:31:32 +0200

    added controlled form

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import About from './About';
 import Menu from './Menu';
 import Contact from './Contact';
 import DishDetail from './DishDetail';
@@ -55,6 +56,10 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
+          <Route
+            path="/aboutus"
+            component={() => <About leaders={this.state.leaders} />}
+          />
           <Route
             exact
             path="/menu"
