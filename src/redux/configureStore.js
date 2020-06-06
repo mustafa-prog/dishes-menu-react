6d22707e28e808352538774b6465dd73302460a1
Mustafa Kurukamci <isbuan51@gmail.com>
2020-06-06 21:50:28 +0200

    added redux-thunk and implemented with setTimeOut function, redux-logger, added Loading Component(spinner),updated other component's view

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 const rootReducer = combineReducers({
   dishes: Dishes,
@@ -14,7 +16,11 @@ const rootReducer = combineReducers({
 export const ConfigureStore = () => {
   const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    compose(
+      applyMiddleware(thunk, logger),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
   );
   return store;
 };
