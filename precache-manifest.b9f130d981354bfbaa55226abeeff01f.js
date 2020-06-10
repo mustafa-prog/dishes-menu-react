0f0f736a6682bb4fcbb22a3f20079136f267fbe1
Mustafa Kurukamci <isbuan51@gmail.com>
2020-06-10 12:31:57 +0200

    Updates

---
+++ precache-manifest.b9f130d981354bfbaa55226abeeff01f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc520df45a4e81c99da939d9031d3ac",
+    "url": "/website-with-react/index.html"
+  },
+  {
+    "revision": "dc830d36673a3a69656c",
+    "url": "/website-with-react/static/css/2.5b499caa.chunk.css"
+  },
+  {
+    "revision": "f0ef6c67e0b12ad3f54c",
+    "url": "/website-with-react/static/css/main.23e744cf.chunk.css"
+  },
+  {
+    "revision": "dc830d36673a3a69656c",
+    "url": "/website-with-react/static/js/2.c0b41442.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/website-with-react/static/js/2.c0b41442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ef6c67e0b12ad3f54c",
+    "url": "/website-with-react/static/js/main.e9ce0991.chunk.js"
+  },
+  {
+    "revision": "a1e4373d729fb7bc84dc",
+    "url": "/website-with-react/static/js/runtime-main.f4fe912a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/website-with-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/website-with-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/website-with-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/website-with-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/website-with-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
